8b840eaeb56d58458d52c41aa732252ea7c6823b
Alan Paul <allanpaulthe@gmail.com>
2018-03-11 23:57:13 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -20,6 +20,15 @@ import teacherHome from './components/teacherhome';
 import semList from './components/semList';
 import selfA from './components/selfanalysis';
 import totalA from './components/totalAnalysis';
+import Chart from './components/chart';
+import stuSemList from './components/stuSemList';
+import semCompare1 from './components/semComapare';
+import semChart from './components/semChart';
+import batchList from './components/batchList';
+import excelPage from './components/excelpage';
+import subjectAnalysis from './components/subAna';
+import update from './components/update';
+
 
 export const results = StackNavigator({
   Home: { screen: LoginScreen,
@@ -27,53 +36,27 @@ export const results = StackNavigator({
       header: null,
     },
   },
-  HomeScreen: { screen: Home, title: "Second Screen",
-  navigationOptions: {
-    header: null,
-  },
- },
+  HomeScreen: { screen: Home},
   myhome: { screen: myhome, title: "Second Screen" },
   SecondScreen: { screen: SecondScreen, title: "Second Screen" },
-  teachersignupScreen: { screen: teachersignupScreen,navigationOptions: {
-    header: null,
-    },
-  },
-  studentsignupScreen: { screen: studentsignupScreen,navigationOptions: {
-    header: null,
-    },
-  },
-  collegehome: { screen: collegeHome,navigationOptions: {
-    header: null,
-    },
-  },
-  collegesignupScreen: { screen: collegesignupScreen,navigationOptions: {
-    header: null,
-    },
-  },
-  signupList: { screen: signupList,navigationOptions: {
-    header: null,
-    },
-  },
-  uploadPdf: { screen: uploadPdf,navigationOptions: {
-    header: null,
-    },
-  },
-  teacherHome: { screen: teacherHome,navigationOptions: {
-    header: null,
-    },
-  },
-  semList: { screen: semList,navigationOptions: {
-    header: null,
-    },
-  },
-  selfA: { screen: selfA,navigationOptions: {
-    header: null,
-    },
-  },
-  totalA: { screen: totalA,navigationOptions: {
-    header: null,
-    },
-  }
+  teachersignupScreen: { screen: teachersignupScreen},
+  studentsignupScreen: { screen: studentsignupScreen},
+  collegehome: { screen: collegeHome},
+  collegesignupScreen: { screen: collegesignupScreen},
+  signupList: { screen: signupList},
+  uploadPdf: { screen: uploadPdf},
+  teacherHome: { screen: teacherHome},
+  semList: { screen: semList},
+  selfA: { screen: selfA},
+  totalA: { screen: totalA},
+  chart: { screen: Chart},
+  stuSemList:{screen:stuSemList},
+  semCompare1:{screen:semCompare1},
+  semChart:{screen:semChart},
+  batchList:{screen:batchList},
+  excelPage:{screen:excelPage},
+  SubAnalysis:{screen:subjectAnalysis},
+  update:{screen:update},
 
 })
 
